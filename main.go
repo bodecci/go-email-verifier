db3fe9b557cb252e6fc90fc1fda2e2e934ff833d
Bode <bode.falade@outlook.com>
2024-09-27 17:28:04 -0500

    added check for dmarcRecords.

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"log"
 	"net"
 	"os"
+	"strings"
 )
 
 func main() {
@@ -23,7 +24,7 @@ func main() {
 
 func checkDomain(domain string) {
 	var hasMX, hasSPF, hasDMARC bool
-	var sprRecord, dmarcRecord string
+	var spfRecord, dmarcRecord string
 
 	mxRecords, err := net.LookupMX(domain)
 	if err != nil {
@@ -32,5 +33,29 @@ func checkDomain(domain string) {
 
 	if len(mxRecords) > 0 {
 		hasMX = true
+	}
+
+	txtRecords, err := net.LookupTXT(domain)
+	if err != nil {
+		log.Printf("Error :%v\n", err)
+	}
+
+	for _, record := range txtRecords {
+		if strings.HasPrefix(record, "v=spf1") {
+			hasSPF = true
+			spfRecord = record
+			break
+		}
+	}
+
+	dmarcRecords, err := net.LookupTXT("_dmarc." + domain)
+	if err != nil {
+		log.Printf("Error %v\n", err)
+	}
+
+	for _, record := range dmarcRecords {
+		if strings.HasPrefix(record, "v=DMARC1") {
+
+		}
 	}
 }
